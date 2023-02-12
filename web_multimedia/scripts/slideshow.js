a55f94b7bcbbe04fe4fa516a04494ba1a5fddc7f
Nikita Melnikov <melnikov.n.89@mail.ru>
2023-02-12 21:38:36 +0300

    first commit

---
+++ web_multimedia/scripts/slideshow.js
@@ -0,0 +1,40 @@
+let timer = 5000;
+
+let timerId = setInterval(plusSlides, timer, 1);
+
+function setTimer(n){
+  clearInterval(timerId);
+  timer = n*1000;
+  document.getElementById("timerLabel").innerText = "Время переключения слайдов: " + n + " секунд";
+  timerId = setInterval(plusSlides, timer, 1);
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n); 
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
